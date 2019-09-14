d97537cbd8e81a838bfe44e7726fd8346e0ea6c1
erisjimver <erisjinver@gmail.com>
2019-09-13 22:21:26 -0500

    funcional CRUD en archivo web

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,29 @@ Route::get("/articulos","MiControlador@store");
 Route::get("/mostrar","MiControlador@show");
 Route::get("/contacto","MiControlador@contactar");
 Route::get("/galeria","MiControlador@galeria");
+
+Route::get("/insertar",function(){
+
+//DB::insert("insert into articulos(nombre_articulo,precio,pais_de_origen_seccion,observaciones) values(?,?,?,?,?)",["jarron","15.2","japon","ceramica","ganga"]);
+
+});
+
+Route::get("/insertar", function(){
+ DB::insert("INSERT INTO articulos (NOMBRE_ARTICULO, PRECIO, PAIS_DE_ORIGEN, SECCION, OBSERVACIONES) VALUES(?,?,?,?,?)",["JARRON",15.2,"JAPON","CERAMICA","GANGA"]);
+});
+
+Route::get('/leer', function(){
+    $resultados=DB::select("SELECT * FROM  articulos WHERE id=?", [2]);
+	//$resultados=DB::update("select nombre_articulo from articulos where id=1");
+    foreach($resultados as $articulos){
+    	return $articulos->nombre_articulo;
+    }
+});
+
+Route::get('/actualizar', function(){
+    DB::update("update articulos set seccion='Decoracion' WHERE ID=?", [2]);
+});
+
+Route::get('/borrar', function(){
+    DB::delete("DELETE FROM articulos WHERE ID=?", [1]);
+});
